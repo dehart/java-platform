a4cfb6ccb3fe397c1af7412c67aa4134a8b65bf5
Michael de Hart <mdhart@mail.intermesh.dev>
2015-01-13 18:29:23 +0100

    hand.shake

--- src/jellyfish/Jellyfish.java
+++ src/jellyfish/Jellyfish.java
@@ -10,6 +10,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import jellyfish.util.Base64;
 
 public class Jellyfish extends Thread {
 
@@ -21,7 +24,6 @@ public class Jellyfish extends Thread {
 //        serverSocket = new ServerSocket(port);
 //        serverSocket.setSoTimeout(10000);
 //    }
-
 //    public void run() {
 //        while (true) {
 //            try {
@@ -43,7 +45,6 @@ public class Jellyfish extends Thread {
 //            }
 //        }
 //    }
-
 //    public static void main(String[] args) {
 ////      int port = Integer.parseInt(args[0]);
 //        int port = 6066;
@@ -54,57 +55,86 @@ public class Jellyfish extends Thread {
 //            e.printStackTrace();
 //        }
 //    }
-    
-    
-    public static void main(String[] args) throws InterruptedException  {
+	public static void main(String[] args) throws InterruptedException {
 
 		new Thread(new SimpleServer()).start();
-		new Thread(new SimpleClient()).start();
-                
+		//new Thread(new SimpleClient()).start();
+
 //                Thread.sleep(20000);
 //                new Thread(new SimpleClient()).start();
-
 	}
+
+	static class Hand {
+
+		public Hand() {
+			
+		}
+		
+		public String shake(String in) {
+			return generateFinalKey(in);
+		}
+
+		private String generateFinalKey(String in) {
+			String seckey = in.trim();
+			String acc = seckey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+			MessageDigest sh1;
+			try {
+				sh1 = MessageDigest.getInstance("SHA1");
+			} catch (NoSuchAlgorithmException e) {
+				throw new RuntimeException(e);
+			}
+			return Base64.encodeBytes(sh1.digest(acc.getBytes()));
+		}
+	}
+	
+	
 
 	static class SimpleServer implements Runnable {
 
 		@Override
 		public void run() {
-
+			Hand hand = new Hand();
 			ServerSocket serverSocket = null;
+			
 			try {
+
+				serverSocket = new ServerSocket(12345);
 				
-				serverSocket = new ServerSocket(3333);
-                                
 //				serverSocket.setSoTimeout(7000);
-				
-				
+				try {
+					Socket clientSocket = serverSocket.accept();
 
-					try {
-						Socket clientSocket = serverSocket.accept();
-                                                
-                                                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-						BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+					BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 //						DataInputStream inputReader = new DataInputStream(clientSocket.getInputStream());
 //                                                while (true) {
 //                                                    System.out.println("Client said :"+ inputReader.readLine());
 //                                                }
-                                                
-                                                String inputLine; 
-                                                while ((inputLine = inputReader.readLine()) != null) 
-                                                { 
-                                                 System.out.println ("Server: " + inputLine); 
-                                                 //out.println(inputLine); 
 
-                                                 if (inputLine.equals("Bye.")) 
-                                                     break; 
-                                                }
+					String inputLine;
+					while ((inputLine = inputReader.readLine()) != null) {
+						System.out.println("Server: " + inputLine);
+						//out.println(inputLine); 
+						if(inputLine.contains("Sec-WebSocket-Key: ")){
+							
+							out.println("HTTP/1.1 101 Switching Protocols");
+							out.println("Upgrade: websocket");
+							out.println("Connection: Upgrade");
+							out.println("Sec-WebSocket-Accept: "+hand.shake(inputLine.substring(19)));
+							//out.println("Sec-WebSocket-Protocol: chat");
+							out.println("\n");
+							//out.flush();
+						}
 
-                                                
-					} catch (SocketTimeoutException e) {
-						e.printStackTrace();
-					
+						if (inputLine.equals("Bye.")) {
+							break;
+						}
+					}
+
+				} catch (SocketTimeoutException e) {
+					e.printStackTrace();
+
 				}
 
 			} catch (SocketTimeoutException e) {
@@ -113,8 +143,9 @@ public class Jellyfish extends Thread {
 				e.printStackTrace();
 			} finally {
 				try {
-					if (serverSocket != null)
+					if (serverSocket != null) {
 						serverSocket.close();
+					}
 
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -156,8 +187,9 @@ public class Jellyfish extends Thread {
 			} finally {
 
 				try {
-					if (socket != null)
+					if (socket != null) {
 						socket.close();
+					}
 				} catch (IOException e) {
 
 					e.printStackTrace();
